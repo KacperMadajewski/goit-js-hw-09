bdcdafc1879aee54eff0251449fe5f2d4b5c0cc5
KacperMadajewski <kacpermadajewski@gmail.com>
2023-03-22 00:08:59 +0100

    Done exercise 3

--- dist/03-promises.cd58c9ff.js
+++ dist/03-promises.cd58c9ff.js
@@ -503,10 +503,36 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"8tzFL":[function(require,module,exports) {
-function createPromise(position, delay) {
-    const shouldResolve = Math.random() > 0.3;
-    shouldResolve;
-}
+const form = document.querySelector(".form");
+const createPromise = (position, delay)=>{
+    return new Promise((resolve, reject)=>{
+        let time = setTimeout(()=>{
+            const shouldResolve = Math.random() > 0.3;
+            if (shouldResolve) resolve({
+                position,
+                delay
+            });
+            else reject({
+                position,
+                delay
+            });
+        }, delay);
+    });
+};
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const valueAmount = Number(form.elements.amount.value);
+    const valueStep = Number(form.elements.step.value);
+    const valueDelay = Number(form.elements.delay.value);
+    for(let i = 1; i <= valueAmount; i += 1){
+        let time = valueDelay + valueStep * (i - 1);
+        createPromise(i, time).then(({ position , delay  })=>{
+            console.log(`✅ Fulfilled promise ${position} in ${delay} ms`);
+        }).catch(({ position , delay  })=>{
+            console.log(`❌ Rejected promise ${position} in ${delay} ms`);
+        });
+    }
+});
 
 },{}]},["dDjGg","8tzFL"], "8tzFL", "parcelRequired7c6")
 
